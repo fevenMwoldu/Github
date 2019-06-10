cfa900ab19235eef13a7611dfd96bfd89f3132d0
Feven Weldu <feven.m.woldu@gmail.com>
2019-06-10 00:44:10 -0500

    eniv prod and path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  githubUser: 'fevenMwoldu',
+  githubToken: '6ea24c667e7a1b4df46c1d8782d8988663da3005',
+  githubUserSearchBaseUrl: 'https://api.github.com/search/users?q=',
+  githubRepoSearchBaseUrl: 'https://api.github.com/search/repositories?q='
 };
